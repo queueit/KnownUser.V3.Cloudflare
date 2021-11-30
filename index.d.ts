518993e66502db3a2ae2b65943778f1cc432f2c0
Vasil <vava@queue-it.com>
2021-11-30 12:46:06 +0100

    Remove old JavaScript files

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function setIntegrationDetails (customerId: string, secretKey: string): void;
-export declare function onClientRequest(request: any): any;
-export declare function onClientResponse(response: any): any;
